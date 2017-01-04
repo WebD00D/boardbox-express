b5e75ef79cc7c1fca38ab569ab3af88d9221ac2b
Christian Bryant <christian.bryant@mobelux.com>
2017-01-03 21:19:27 -0500

    updates server for heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 })
 
-app.listen(8080, function () {
+app.listen(process.env.PORT || 8080, function () {
   console.log('Listening on port 8080!')
 })
 
